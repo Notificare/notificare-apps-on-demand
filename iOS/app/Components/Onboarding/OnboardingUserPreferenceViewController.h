4b0548b739b30c4b192cd88d0dcfb06ace2de12e
Aernout Peeters <aernout@gmail.com>
2016-01-12 23:03:57 +0100

    Updated Configuration.plist appSettings entry.

---
+++ iOS/app/Components/Onboarding/OnboardingUserPreferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  OnboardingUserPreferenceViewController.h
+//  app
+//
+//  Created by Aernout Peeters on 12-01-2016.
+//  Copyright © 2016 Notificare. All rights reserved.
+//
+
+#import "NotificareComponentViewController.h"
+
+@interface OnboardingUserPreferenceViewController : NotificareComponentViewController
+
+@end
